e84cd834ef042b35bd214f6323b0fc7d4eceecfa
SunnyYajie <131513384+SunnyYajie@users.noreply.github.com>
2024-02-23 23:22:39 +0800

    2-23-24 Commit-Added Scroll Animation

--- script.js
+++ script.js
@@ -318,3 +318,18 @@ function onMouseLeave() {
     pointerY = null;
 
 }
+
+/* ========================== Scroll Animation ========================== */
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
